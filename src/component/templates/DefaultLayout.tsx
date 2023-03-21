63da4683e444c21834cf5b55cad5e43a466d785b
Toshiki Kawazoe <bushy.trivial.0o@icloud.com>
2023-03-22 00:00:11 +0900

    Add default layout

---
+++ src/component/templates/DefaultLayout.tsx
@@ -0,0 +1,32 @@
+import React, { useState, ReactNode } from  "react";
+import { Box, TextField, Button, Grid, Container } from "@mui/material";
+import { styled } from '@mui/material/styles';
+
+import SideBer from '../organisms/SideBer';
+import Search from '../molecules/Search';
+import Control from '../organisms/Control';
+import PurchaseList from '../organisms/PurchaseList';
+
+type Row = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number | null;
+};
+
+type Props = {
+  children: ReactNode
+}
+
+
+export default function DefaultLayout(props: Props) {
+  return (
+    <Box sx={{display: "flex"}}>
+      <SideBer/>
+      <Container fixed sx={{ m: 4}}>
+        {props.children}
+        
+      </Container>
+    </Box>
+  );
+}
